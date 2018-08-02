a9cf951f9a3755fc295f8bfc7d0e72635dca358e
katherinefy <785026981@qq.com>
2018-08-02 09:59:17 +0800

    组件创建和组件之间传值

---
+++ demo/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <firstComponents text="计数" @add="addCount"></firstComponents>
+    <firstComponents text="计数" @add="addCount"></firstComponents>
+
+    <p>按钮被点击的总次数：{{totalCount}}</p>
+  </div>
+</template>
+
+<script>
+import myButton from './components/Buttons/Button'
+
+export default {
+  name: 'App',
+  data:function(){
+       return{
+             "totalCount":0
+           }
+         },
+  methods:{
+    "addCount":function(){
+      this.totalCount++;
+    }
+  },
+  components:{
+    "firstComponents":myButton
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ demo/src/components/Buttons/Button.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+    <button class="ui-button" @click="addCount">{{text}}</button>
+    <p>按钮被点击了{{count}}次</p>
+    </div>
+</template>
+<script>
+    export default {
+        name:"myButton",
+        data:function(){
+          return{
+            "message":"my first component",
+            "count":0
+          }
+        },
+        methods:{
+          "addCount":function(){
+            this.count ++;
+            this.$emit("add");
+          }
+        },
+        props:{
+          'text':{
+            type:String,
+            required:true
+          }
+        }
+    }
+</script>
+<style>
+.ui-button{
+  background-color:blue;
+  color:white;
+  border:none;
+}
+</style>
